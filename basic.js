42c37bd87ed1768e07455b59542e5946cf48566b
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-11-14 08:05:55 -0600

    javascript menu implemented in home page

--- basic.js
+++ basic.js
@@ -1,2 +1,13 @@
-// Elements defined here
-hamburger = document.getElementById("hamburger");
+let hamburger = document.getElementById("hamburger");
+let ham_close = document.getElementById("ham-close");
+let overlay = document.querySelector(".overlay");
+let navigation = document.querySelector("#navigation");
+
+function showing(){
+    overlay.classList.toggle("mobile-hidden");
+    navigation.classList.toggle("mobile-hidden");
+}
+
+hamburger.addEventListener("click", showing);
+ham_close.addEventListener("click", showing);
+overlay.addEventListener("click", showing);
