3fb014c99952c0a8676c10f8fe214a71768f7170
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-11-15 13:42:12 -0600

    fixed mobile menu

--- basic.js
+++ basic.js
@@ -2,10 +2,12 @@ let hamburger = document.getElementById("hamburger");
 let ham_close = document.getElementById("ham-close");
 let overlay = document.querySelector(".overlay");
 let navigation = document.querySelector("#navigation");
+let classier = document.getElementById("classier");
 
 function showing(){
     overlay.classList.toggle("mobile-hidden");
     navigation.classList.toggle("mobile-hidden");
+    classier.classList.toggle("mobile-hidden");
 }
 
 hamburger.addEventListener("click", showing);
