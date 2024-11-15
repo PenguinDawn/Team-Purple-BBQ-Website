a169fab205049f366d2cb4258fe30b670086bb93
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-11-15 14:09:14 -0600

    fixed dropdown menu

--- basic.js
+++ basic.js
@@ -13,3 +13,21 @@ function showing(){
 hamburger.addEventListener("click", showing);
 ham_close.addEventListener("click", showing);
 overlay.addEventListener("click", showing);
+
+
+// Showing the dropdown
+
+
+const dropper = document.getElementById("dropper");
+const dropped = document.getElementById("dropped");
+const over = document.getElementById("outside-clicker");
+
+function hide(){
+    dropped.classList.toggle("hidden")
+    dropper.classList.toggle("open");
+    over.classList.toggle("hidden");
+}
+
+dropper.addEventListener("click", hide);
+over.addEventListener("click", hide);
+
