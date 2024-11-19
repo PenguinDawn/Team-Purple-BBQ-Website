84a40d94c08175eade3de791d59a39690a21a770
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-11-19 08:05:10 -0600

    schedule working

---
+++ schedule/sched.js
@@ -0,0 +1,19 @@
+{/* <div class="card col">
+                        <div class="card-collapsed row">
+                            <img src="../images/star-outline.svg" id="arrow" alt="Favorite star">
+                            <p class="time">6:00 pm</p>
+                            <h3 class="event-name">Touch-a-Truck and First Responders Appreciation</h3>
+                            <img src="../images/Arrow.svg" id="arrow" alt="collapse arrow">
+                        </div>
+                        <div class="card-content">
+                            <p class="description">Bring the kids to enjoy a hands-on event with Emergency and Utility Vehicles.</p>
+                            <p class="location"><span class="blue">Location:</span> Henderson Church of Christ parking lot</p>
+                            <p class="price"><span class="blue">Price:</span> Free</p>
+                            <p class="sponsor"><span class="blue">Sponsor:</span> Jones Contractors</p>
+                        </div>
+                    </div> */}
+
+const headers = document.querySelectorAll(".card-collapsed");
+const arrows = document.querySelectorAll("arrow");
+
+// NextElementSibling for card-content
