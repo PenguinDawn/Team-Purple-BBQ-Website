870e2254221291ba3016c55d81ba61f37e33d6e5
PenguinDawn <157507535+PenguinDawn@users.noreply.github.com>
2024-11-19 11:37:39 -0600

    accordion done

--- schedule/sched.js
+++ schedule/sched.js
@@ -1,19 +1,27 @@
-{/* <div class="card col">
-                        <div class="card-collapsed row">
-                            <img src="../images/star-outline.svg" id="arrow" alt="Favorite star">
-                            <p class="time">6:00 pm</p>
-                            <h3 class="event-name">Touch-a-Truck and First Responders Appreciation</h3>
-                            <img src="../images/Arrow.svg" id="arrow" alt="collapse arrow">
-                        </div>
-                        <div class="card-content">
-                            <p class="description">Bring the kids to enjoy a hands-on event with Emergency and Utility Vehicles.</p>
-                            <p class="location"><span class="blue">Location:</span> Henderson Church of Christ parking lot</p>
-                            <p class="price"><span class="blue">Price:</span> Free</p>
-                            <p class="sponsor"><span class="blue">Sponsor:</span> Jones Contractors</p>
-                        </div>
-                    </div> */}
-
 const headers = document.querySelectorAll(".card-collapsed");
-const arrows = document.querySelectorAll("arrow");
+let opened_head = "";
+let open = false;
 
-// NextElementSibling for card-content
+headers.forEach(header => {
+    header.addEventListener("click", () => {
+
+        if(header.classList.contains("active")) {
+            open = false;
+        } 
+        else if (open == true) {
+            opener = document.querySelector(".active")
+            opener.querySelector(".arrow").classList.toggle("rotated");
+            opener.nextElementSibling.classList.toggle("hidden");
+            opener.classList.toggle("active");
+        }
+        else {
+            open = true;
+        }
+        
+
+
+        header.querySelector('.arrow').classList.toggle("rotated");
+        header.nextElementSibling.classList.toggle("hidden");
+        header.classList.toggle("active");
+    })});
+
